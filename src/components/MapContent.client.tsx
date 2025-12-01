3a24ef15e91c9df30f57b6c11b50aeaa304e1f17
ljh981009 <ljh981009@naver.com>
2025-12-01 17:29:00 -0500

    fix lint code error

--- src/components/MapContent.client.tsx
+++ src/components/MapContent.client.tsx
@@ -2,29 +2,33 @@
 
 import { MapContainer, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import L from "leaflet";
+import L, { Icon } from "leaflet";
 import { useEffect } from "react";
 
 export default function MapContent() {
   useEffect(() => {
-    // Fix for default marker icon issues in Next.js
-    delete (L.Icon.Default.prototype as any)._getIconUrl;
-    L.Icon.Default.mergeOptions({
-      iconRetinaUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
-      iconUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
-      shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
+    (Icon.Default.prototype as unknown as { _getIconUrl?: unknown })._getIconUrl = undefined;
+
+    Icon.Default.mergeOptions({
+      iconRetinaUrl:
+        "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon-2x.png",
+      iconUrl:
+        "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon.png",
+      shadowUrl:
+        "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-shadow.png",
     });
   }, []);
 
   return (
-    <MapContainer 
-      center={[43.6532, -79.3832]} 
-      zoom={13} 
+    <MapContainer
+      center={[43.6532, -79.3832]} // Toronto
+      zoom={13}
+      scrollWheelZoom
       style={{ height: "100%", width: "100%" }}
       className="z-0"
     >
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
     </MapContainer>
